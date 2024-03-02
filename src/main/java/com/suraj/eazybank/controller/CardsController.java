9b46bd720388441cc0020e916e1e3f7dec0fd539
Suraj <surajkala2001@gmail.com>
2024-03-02 21:41:35 +0530

    Basic Setup

---
+++ src/main/java/com/suraj/eazybank/controller/CardsController.java
@@ -0,0 +1,14 @@
+package com.suraj.eazybank.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CardsController {
+	
+	@GetMapping("/myCards")
+	
+	public String getCardDetails() {
+		return "Here are the Card details from DB";
+	}
+}
---
+++ src/main/java/com/suraj/eazybank/controller/NoticesController.java
@@ -0,0 +1,13 @@
+package com.suraj.eazybank.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NoticesController {
+	
+	@GetMapping("/notices")
+	public String getNotices() {
+		return "Here are the notice details from the DB";
+	}
+}
